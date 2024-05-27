e24be9df1e3b16009997fb7951d2640989798ba2
FRAN IRICOM <fran.villar.huerga@outlook.com>
2024-05-27 14:12:06 +0200

    c

---
+++ BoteForms/modelo/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+    public class Usuario
+    {
+        public int UsuarioID { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+
